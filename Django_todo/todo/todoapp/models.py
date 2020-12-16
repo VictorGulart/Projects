12844606062761667d95e08c87368f9b95a3f275
Floki <victorlgulart@gmail.com>
2020-12-16 10:12:42 +0000

    added user to models to start implementing credentials authorization.

--- Django_todo/todo/todoapp/models.py
+++ Django_todo/todo/todoapp/models.py
@@ -5,18 +5,20 @@ from django.utils import timezone
 # Create your models here.
 
 
-# class User(models.Model):
-#     username = models.TextField()
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.TextField()
+    email = models.EmailField(blank=True, null=True)
 
 class Task(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(default='Add Title', max_length=200)
     message = models.TextField(default='Add Notes')
     pub_date = models.DateTimeField(default=timezone.now)
     checked = models.BooleanField(default=False)
 
     def __str__( self ):
-        return str( self.user )
+        return str( self.title )
     
     @property
     def owner( self ):
